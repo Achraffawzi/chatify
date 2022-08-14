4a5e59fd781b5ddafd4a9d60bc8f1fc47c893f45
delphine <delphin38@live.fr>
2022-08-14 11:40:35 +0200

    [Home] John Doe - Profile

--- client/src/pages/auth/home/Home.js
+++ client/src/pages/auth/home/Home.js
@@ -2,11 +2,24 @@ import React from 'react';
 
 import "./Home.scss";
 
+import Profil from "../../../components/images/profil.png";
+
 const Home = () => {
   return (
     <div>
-        <p className='home_pseudo'>John Doe</p>
-       <a href='#'><p>Profil</p></a> 
+      <section className='home_profil'>
+
+      
+      <article className='home_image'>
+        <img src={Profil} alt="profil" />
+      </article>
+      
+
+      <article className='home_pseudo_a'>
+        <p className='home_pseudo'>John Doe <br></br>
+       <a href='#' className='home_a_profil'>Profile</a> </p>
+      </article>
+        </section>
       
     </div>
   )
